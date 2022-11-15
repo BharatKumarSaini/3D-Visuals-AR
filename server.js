1b7cb7d8e4853813a24a2061de79aee865b340c6
BharatKumarSaini <bharatskataria@gmail.com>
2022-11-15 21:38:40 +0530

    fix: custom model rendering on preset marker

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-import express from "express";
+const express = require("express");
 const app = express();
-import path from "path";
+const path = require("path");
+const cors = require("cors");
+app.use(cors());
 
 app.set("/", "html");
 app.use(express.static(path.join(__dirname, "/")));
